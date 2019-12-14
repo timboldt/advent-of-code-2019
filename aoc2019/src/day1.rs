e3b2010f5b9695c4fe7ee8a581dbd9af5d789e85
Tim Boldt <tim.boldt@gmail.com>
2019-12-14 00:18:17 -0800

    Finished Day 1 in Rust.

--- aoc2019/src/day1.rs
+++ aoc2019/src/day1.rs
@@ -13,7 +13,16 @@ pub fn part1(masses: &[i32]) -> i32 {
 
 #[aoc(day1, part2)]
 pub fn part2(masses: &[i32]) -> i32 {
-    42
+    masses.iter().map(|m| part2_fuel(*m)).sum()
+}
+
+pub fn part2_fuel(mass: i32) -> i32 {
+    let fuel = mass / 3 - 2;
+    if fuel <= 0 {
+        0
+    } else {
+        fuel + part2_fuel(fuel)
+    }
 }
 
 #[cfg(test)]
@@ -21,22 +30,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn part1_example1() {
+    fn part1_examples() {
         assert_eq!(part1(&[12]), 2);
-    }
-
-    #[test]
-    fn part1_example2() {
         assert_eq!(part1(&[14]), 2);
-    }
-
-    #[test]
-    fn part1_example3() {
         assert_eq!(part1(&[1969]), 654);
+        assert_eq!(part1(&[100756]), 33583);
     }
 
     #[test]
-    fn part1_example4() {
-        assert_eq!(part1(&[100756]), 33583);
+    fn part2_examples() {
+        assert_eq!(part2(&[12]), 2);
+        assert_eq!(part2(&[14]), 2);
+        assert_eq!(part2(&[1969]), 966);
+        assert_eq!(part2(&[100756]), 50346);
     }
 }
