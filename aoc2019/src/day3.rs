3521621d64f70b7325904b4bc305fa3fe9b791c5
Tim Boldt <tim.boldt@gmail.com>
2019-12-15 19:23:32 -0800

    Day 3 parsing

---
+++ aoc2019/src/day3.rs
@@ -0,0 +1,107 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct WireSegment {
+    direction: Direction,
+    distance: usize,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Wire(Vec<WireSegment>);
+
+impl FromStr for WireSegment {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut op_str = String::from(s);
+        let dist_str = op_str.split_off(1);
+        let dir = match op_str.chars().next().unwrap_or('*') {
+            'U' => Direction::Up,
+            'D' => Direction::Down,
+            'L' => Direction::Left,
+            'R' => Direction::Right,
+            _ => unreachable!(),
+        };
+        let dist = dist_str.parse()?;
+
+        Ok(WireSegment {
+            direction: dir,
+            distance: dist,
+        })
+    }
+}
+
+#[aoc(day3, part1)]
+pub fn part1(input: &str) -> usize {
+    let mut lines = input.lines();
+    let wire1 = Wire(
+        lines
+            .next()
+            .unwrap_or("")
+            .split(",")
+            .map(|n| WireSegment::from_str(n).unwrap())
+            .collect(),
+    );
+    let wire2 = Wire(
+        lines
+            .next()
+            .unwrap_or("")
+            .split(",")
+            .map(|n| WireSegment::from_str(n).unwrap())
+            .collect(),
+    );
+
+    println!("Wire2 {:?}", wire2);
+    42
+}
+
+// #[aoc(day3, part2)]
+// pub fn part2(input: &[usize]) -> usize {
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsing() {
+        assert_eq!(
+            Op::from_str("U99").unwrap(),
+            Op {
+                direction: Direction::Up,
+                distance: 99
+            }
+        );
+        assert_eq!(
+            Op::from_str("D1").unwrap(),
+            Op {
+                direction: Direction::Down,
+                distance: 1
+            }
+        );
+        assert_eq!(
+            Op::from_str("L12345").unwrap(),
+            Op {
+                direction: Direction::Left,
+                distance: 12345
+            }
+        );
+        assert_eq!(
+            Op::from_str("R0").unwrap(),
+            Op {
+                direction: Direction::Right,
+                distance: 0
+            }
+        );
+    }
+}
